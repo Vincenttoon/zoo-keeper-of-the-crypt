ce7d63ce44138b75e74606275c1bd3cc69937c63
Vincent Toon <vincenttoon22@gmail.com>
2022-09-23 00:32:43 -0500

    front-end functioning

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const { animals } = require("./data/animals");
+const { rawListeners } = require("process");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static('public'));
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -114,6 +117,22 @@ app.post("/api/animals", (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
